8b886966fdbc090187b5bac99f71b5ecc3a915b7
Max Miller <maxmiller413@gmail.com>
2021-05-03 01:04:16 -0500

    login, signup, navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
     has_many :wishlists
+    
     has_secure_password
+
+    validates :username, uniqueness: { case_sensitive: false}
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+
+# wishlist_places
   resources :wishlist_places
+
+# wishlists
   resources :wishlists
+
+# places
   resources :places
-  resources :users
+
+# resources :users
+  #users
+  post "/login", to: "users#login" 
+  post "/signup", to: "users#signup"
+  get "/me/:id", to: "users#show"
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

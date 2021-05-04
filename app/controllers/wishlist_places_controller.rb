615a0e69d1cd910f395b0e3add28b78959f07bd3
Max Miller <maxmiller413@gmail.com>
2021-05-04 11:25:15 -0500

    updated wishlists

--- app/controllers/wishlist_places_controller.rb
+++ app/controllers/wishlist_places_controller.rb
@@ -8,4 +8,15 @@ class WishlistPlacesController < ApplicationController
         @wishlist_place = WishlistPlace.find_by(id: params[:id])
         render json: @wishlist_place
     end 
+
+    def create
+        @wishlist_place = WishlistPlace.create(wishlist_place_params)
+        render json: @wishlist_place
+    end
+
+    private
+
+    def wishlist_place_params
+        params.require(:wishlist_place).permit(:wishlist_id, :place_id)
+    end
 end
--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -8,4 +8,15 @@ class WishlistsController < ApplicationController
         @wishlist = Wishlist.find_by(id: params[:id])
         render json: @wishlist
     end 
+
+    def create
+        @wishlist = Wishlist.create(wishlist_params)
+        render json: @wishlist
+    end
+
+    private
+
+    def wishlist_params
+        params.require(:wishlist).permit(:title, :user_id)
+    end
 end

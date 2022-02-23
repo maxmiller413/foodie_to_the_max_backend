0bf607308161eb3767d1747de3f87d6bff065b00
Max Miller <maxmiller413@gmail.com>
2022-02-22 21:05:44 -0600

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,6 +5,8 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
+# add specific origin or white label?
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
